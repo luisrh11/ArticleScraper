b4a6296b4073759fc3938bb1e8ce95932d81d83e
luis <hamburger120@outlook.com>
2019-04-11 20:25:01 -0400

    update

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get("/home", function(req, res){
 
 // Get route that gets all of the articles
 app.get("/scrape", function (req, res) {
+  // res.render("index", {title: "Article Scraper"})
   axios.get("https://www.cnn.com/us").then(function (result) {
     // console.log(result);
 
@@ -67,17 +68,30 @@ app.get("/scrape", function (req, res) {
       db.Article.create(resp)
       .then(function(dbArticle){
         // logs the new article
-        console.log(dbArticle);
+        // console.log(dbArticle);
       })
       .catch(function(err){
       // this log the eer if an error occurred
-        console.log(err);
+        // console.log(err);
       })
     })
     // send a message to the client
-    // res.send("scrape complete");
+    // res.alert("scrape complete");  
   })
 })
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 
 // Start the server
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,16 @@
-$("#button1").on("click", function() {
+$("#button1").on("click", function () {
     // console.log("hello!!!");
     location.assign("/scrape");
+
+    $.getJSON("/articles", function (data) {
+        // For each one
+        for (var i = 0; i < data.length; i++) {
+            // Display the apropos information on the page
+            $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        }
+    });
 });
 
-$('#clear').click(function(){
+$('#clear').click(function () {
     // console.log("safsfsafasf");
 })
