ebecf8eab4eb09cc6d257dc73a37bcfbe1d5114a
luis <hamburger120@outlook.com>
2019-04-20 10:10:06 -0400

    fixed1

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/public'));
 
 const MONGODB_URI= process.env.MONGODB_URI || "mongodb://localhost/scrapper"
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 // routes
 
